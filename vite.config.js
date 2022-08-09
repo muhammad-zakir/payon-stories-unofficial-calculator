709be145b10f0e6fc9dd21c7e34e29dc9a77c5a2
jalu <javidevictoria@gmail.com>
2022-08-09 19:17:06 +0200

    build enum path

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { copyFiles } from "./src/utils/plugins/copy-files";
 
 export default defineConfig({
     plugins: [
-        copyFiles('./src/utils/enums', './public/enums'),
+        copyFiles('./src/utils/enums', './public/src/enums'),
         copyFiles('./src', './public/src')
     ]
 });
