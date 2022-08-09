96049f4bed13f376cdf2690b784dd98ee93d11d5
jalu <javidevictoria@gmail.com>
2022-08-09 15:19:16 +0200

    enum as js test, removed extra mobs

---
+++ src/utils/enums/race.enum.js
@@ -0,0 +1,26 @@
+// * TS
+// export enum Race {
+//     Formless,
+//     Undead,
+//     Brute,
+//     Plant,
+//     Insect,
+//     Fish,
+//     Demon,
+//     DemiHuman,
+//     Angel,
+//     Dragon
+// }
+
+const Race = {
+    Formless: 1,
+    Undead: 2,
+    Brute: 3,
+    Plant: 4,
+    Insect: 5,
+    Fish: 6,
+    Demon: 7,
+    DemiHuman: 8,
+    Angel: 9,
+    Dragon: 10
+}
