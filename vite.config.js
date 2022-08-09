b8e2c76f51852925b0ec316fab13fb4d401b163b
jalu <javidevictoria@gmail.com>
2022-08-09 19:19:39 +0200

    path changed again

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from "vite"
-import { copyFiles } from "./src/utils/plugins/copy-files";
+import { copyFiles } from "./src/plugins/copy-files";
 
 export default defineConfig({
     plugins: [
-        copyFiles('./src/utils/enums', './public/src/enums'),
+        copyFiles('./src/enums', './public/src/enums'),
         copyFiles('./src', './public/src')
     ]
 });
